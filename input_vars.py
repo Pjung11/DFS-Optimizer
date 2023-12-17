e53a845b4cca9a40a53a08935536029b8fc5a1f5
PJung11 <pauljung@yahoo.com>
2023-12-17 09:59:28 -0800

    moved inputs to file

---
+++ input_vars.py
@@ -0,0 +1,18 @@
+week = 15
+game_mode = 'classic' # 'showdown' or 'classic'
+rank_src = 'FP' # 'adhoc' or 'FP'
+matchup = 'ALL'
+
+adhoc_pt_col = 'Points' # 'Floor', 'Points', or 'Ceiling'
+
+# to add uncertainty to point projections
+variability_pctg = 11
+
+n_lineups = 25
+add_results = False # To review lineups along with actual outcomes
+
+# scratch players
+scratches = []
+
+# Add must haves in lineup - if forcing defense put space at end (ugh) and use team name
+force_ins = ['Drew Lock']
